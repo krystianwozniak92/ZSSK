2ff3bcd1c0415b65da5e87d79ddee921151594d2
Krystian Wozniak <krystianwozniak92@gmail.com>
2015-05-06 17:48:08 +0200

    Initial project

---
+++ Source/Courier/Courier/Models/Point2D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courier.Models
+{
+    public class Point2D
+    {
+        public int Pos_X { get; set; }
+        public int Pos_Y { get; set; }
+    }
+}
---
+++ Source/Courier/Courier/Models/SalesmanContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Courier.Models;
+
+namespace Courier.Models
+{
+    public class SalesmanContext
+    {
+        public List<Point2D> Points { get; set; }
+    }
+}
