040d19adb1d0d401d2b7571e7673c730c825594d
Krystian Wozniak <krystianwozniak92@gmail.com>
2015-05-17 11:34:50 +0200

    Fixing main

--- Source/CourierDelivery/CourierDelivery/Main.cpp
+++ Source/CourierDelivery/CourierDelivery/Main.cpp
@@ -3,15 +3,21 @@
 #include "SalesmanAnnealing.h"
 #include "SalesmanTabuSearch.h"
 #include "SalesmanGenetic.h"
-
+#include <string>
 using namespace std;
+
+// Function declarations----------------------
+template < class T >
+string GetVectorContent(vector<T>* vector);
+//--------------------------------------------
+
 
 int main()
 {
 	Salesman* salesman = new Salesman(5);
 	Timer timer = Timer();
 
-	// Salesman PARAMETERS----------
+	// Salesman PARAMETERS---------------------
 	// Annealing
 	double lambda = 0.999;
 	// Tabu Search
@@ -20,32 +26,69 @@ int main()
 	int population = 100;
 	int mutationCount = 2;
 	int maxTime = 3;
-	// -----------------------------
+	//-----------------------------------------
 
 	SalesmanAnnealing* sAnnealing = new SalesmanAnnealing(salesman, lambda);
 	SalesmanTabuSearch* sTabuSearch = new SalesmanTabuSearch(salesman, iterations);
 	SalesmanGenetic* sGenetic = new SalesmanGenetic(salesman);
 
+	// Start Salesman algorithms---------------
+	// Measure the execution time of algorithms
+	// Get best cycle (results)
+
+	timer.startTimer();
 	sAnnealing->start();
-	sTabuSearch->start();
-	sGenetic->start(population, mutationCount, maxTime);
-
-	timer.startTimer();
+	timer.stopTimer();
+	double timeSalesAnnealing = timer.getElapsedTime();
 	vector<int>*  resultSalesAnnealing = sAnnealing->GetResult();
-	timer.stopTimer();
-	cout << "Salesman Annealing time: " << timer.getElapsedTime() << endl;
-	
+
 	timer.startTimer();
+	sTabuSearch->start();
+	timer.stopTimer();
+	double timeSalesTabu = timer.getElapsedTime();
 	vector<int>*  resultSalesTabu = sTabuSearch->GetResult();
-	timer.stopTimer();
-	cout << "Salesman Tabu Search time: " << timer.getElapsedTime() << endl;
 
 	timer.startTimer();
-	vector<unsigned>*  resultSalesGenetic = sGenetic->GetResult();
+	sGenetic->start(population, mutationCount, maxTime);
 	timer.stopTimer();
-	cout << "Salesman Genetic time: " << timer.getElapsedTime() << endl;
+	double timeSalesGenetic = timer.getElapsedTime();
+	vector<unsigned>*  resultSalesGenetic = sGenetic->GetResult();
+	//-----------------------------------------
 
+	// Send result to the default output-------
+	cout << "Salesman Annealing:\n";
+	cout << "Best cycle: " << GetVectorContent<int>(resultSalesAnnealing) << endl;
+	cout << "time: " << timeSalesAnnealing << endl;
+
+	cout << "Salesman Tabu Search:\n";
+	cout << "Best cycle: " << GetVectorContent<int>(resultSalesTabu) << endl;
+	cout << "time: " << timeSalesAnnealing << endl;
+
+	cout << "Salesman Genetic:\n";
+	cout << "Best cycle: " << GetVectorContent<unsigned>(resultSalesGenetic) << endl;
+	cout << "time: " << timeSalesAnnealing << endl;
+	//-----------------------------------------
 
 	system("pause");
 	return 0;
 }
+
+
+// Function definitions------------------------
+template < class T >
+string GetVectorContent(vector<T>* vector)
+{
+	string result = "[";
+
+	for (unsigned i = 0; i < vector->size(); i++)
+	{
+		result += to_string((*vector)[i]);
+		if (i < vector->size() -1 )
+			result += ", ";
+	}
+
+	result += "]";
+	return result;
+}
+
+//----------------------------------------------
